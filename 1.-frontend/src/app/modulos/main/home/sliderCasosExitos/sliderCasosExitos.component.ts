959ff8d134ee07efcd39c0f23f95fd24fb940c3d
Your Name <you@example.com>
2018-12-12 19:55:58 -0600

    12 de diciembre 2018 19:55

--- 1.-frontend/src/app/modulos/main/home/sliderCasosExitos/sliderCasosExitos.component.ts
+++ 1.-frontend/src/app/modulos/main/home/sliderCasosExitos/sliderCasosExitos.component.ts
@@ -24,7 +24,7 @@ export class SlidercasosexitosComponent implements OnInit {
     filtro : any;
 
     constructor() {
-        this.slideConfig = { "slidesToShow": 2, "slidesToScroll": 1, "arrows" : false, 'dots' : false, prevArrow: $('.prev'),nextArrow: $('.next') }
+        this.slideConfig = { "slidesToShow": 2, "slidesToScroll": 1, "arrows" : true, 'dots' : false}
         this.currentSlide = 0
         this.filtro = {
                 pagina : 1,
